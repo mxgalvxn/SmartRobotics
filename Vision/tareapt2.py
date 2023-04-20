2e0dbafdef3d0fb02f243a4a7d005b79d1c4083d
ximenaAp002 <A01639678@tec.mx>
2023-04-19 19:17:03 -0600

    Adding second part o part 2, morfologics

--- Vision/tareapt2.py
+++ Vision/tareapt2.py
@@ -1,83 +1,168 @@
 import cv2
 import numpy as np
-import os   
-from PIL import Image
+
+# Cargar las imágenes
+img1 = cv2.imread('edificio1.jpg', 0)
+blur1 = cv2.GaussianBlur(img1, (5, 5), 0)
+edges1 = cv2.Canny(blur1, 100, 200)
+cv2.imshow('Bordes Canny 1', edges1)
 
 
-# Función para resaltar bordes en una imagen en escala de grises
-def resaltar_bordes(img_gris, color_bordes):
-    # Aplicar un filtro de Canny para detectar los bordes
-    bordes = cv2.Canny(img_gris, 100, 200)
-    # Crear una imagen en color con el mismo tamaño que la imagen original
-    img_color = cv2.cvtColor(img_gris, cv2.COLOR_GRAY2BGR)
-    # Superponer los bordes detectados en la imagen en color
-    img_color[bordes != 0] = color_bordes
-    return img_color
+img2 = cv2.imread('edificio2.jpg', 0)
+blur2 = cv2.GaussianBlur(img2, (5, 5), 0)
+edges2 = cv2.Canny(blur2, 100, 200)
+cv2.imshow('Bordes Canny 2', edges2)
 
-# Función para detectar líneas, círculos y polígonos en una imagen en escala de grises
-def detectar_figuras(img_gris):
-    # Aplicar un filtro de Canny para detectar los bordes
-    bordes = cv2.Canny(img_gris, 100, 200)
-    # Aplicar un filtro de Hough para detectar líneas
-    lineas = cv2.HoughLinesP(bordes, 1, cv2.cv2.PI/180, 100, minLineLength=100, maxLineGap=10)
-    # Dibujar las líneas detectadas en una imagen en blanco
-    lineas_img = np.zeros(img_gris.shape, np.uint8)
-    for linea in lineas:
-        x1, y1, x2, y2 = linea[0]
-        cv2.line(lineas_img, (x1, y1), (x2, y2), 255, 2)
-    # Aplicar un filtro de Hough para detectar círculos
-    circulos = cv2.HoughCircles(img_gris, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
-    # Dibujar los círculos detectados en una imagen en blanco
-    circulos_img = np.zeros(img_gris.shape, np.uint8)
-    if circulos is not None:
-        circulos = np.round(circulos[0, :]).astype("int")
-        for (x, y, r) in circulos:
-            cv2.circle(circulos_img, (x, y), r, 255, 2)
-    # Aplicar un filtro de contornos para detectar polígonos
-    _, contornos, _ = cv2.findContours(bordes, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
-    # Dibujar los contornos detectados en una imagen en blanco
-    contornos_img = np.zeros(img_gris.shape, np.uint8)
-    cv2.drawContours(contornos_img, contornos, -1, 255, 2)
-    # Combinar las imágenes de líneas, círculos y contornos en una sola imagen
-    figuras_img = cv2.bitwise_or(lineas_img, circulos_img)
-    figuras_img = cv2.bitwise_or(figuras_img, contornos_img)
-    return figuras_img
 
-# Configurar la captura de video desde la cámara
-cap = cv2.VideoCapture(0)
+img3 = cv2.imread('edificio3.jpg', 0)
+blur3 = cv2.GaussianBlur(img3, (5, 5), 0)
+edges3 = cv2.Canny(blur3, 100, 200)
+cv2.imshow('Bordes Canny 3', edges3)
 
-# Ciclo principal para capturar y procesar cada frame del video
-while True:
-    # Leer el frame actual
-    ret, frame = cap.read()
 
-    # Convertir el frame a escala de grises
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+img4 = cv2.imread('c:/Users/Admin/Desktop/Semestre 6/GitHub/SmartRobotics-main/SmartRobotics-main/Vision/edificio4.jpg', 0)
+blur4 = cv2.GaussianBlur(img4, (5, 5), 0)
+edges4 = cv2.Canny(blur4, 100, 200)
+cv2.imshow('Bordes Canny 4', edges4)
 
-    # Aplicar el detector de bordes Canny al frame en escala de grises
-    edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-    # Convertir los bordes a color rojo y superponerlos en la imagen original
-    edges_color = cv2.cvtColor(edges, cv2.COLOR_GRAY2BGR)
-    edges_color[:,:,0] = 0
-    edges_color[:,:,1] = 0
-    edges_color[:,:,2] = 255
-    frame_edges = cv2.addWeighted(frame, 0.8, edges_color, 1, 0)
+img5 = cv2.imread('edificio5.jpg', 0)
+blur5 = cv2.GaussianBlur(img5, (5, 5), 0)
+edges5 = cv2.Canny(blur5, 100, 200)
+cv2.imshow('Bordes Canny 5', edges5)
+cv2.waitKey(0)
 
-    # Detectar y resaltar líneas, círculos y otros polígonos en el frame en escala de grises
-    canny = cv2.Canny(gray, 50, 150)
-    contours, hierarchy = cv2.findContours(canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(gray, contours, -1, (0,255,0), 2)
+# Aplicar el proceso a cada imagen
+'''for img in [img1, img2, img3, img4, img5]:
+    # Suavizado de la imagen con un filtro Gaussiano
+    blur = cv2.GaussianBlur(img, (5, 5), 0)
 
-    # Mostrar el video en pantalla
-    cv2.imshow('Video original', frame)
-    cv2.imshow('Video con bordes', frame_edges)
-    cv2.imshow('Video con polígonos resaltados', gray)
+    # Detección de bordes con el algoritmo de Canny
+    edges = cv2.Canny(blur, 100, 200)
 
-    # Salir si se presiona la tecla 'q'
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+    # Mostrar la imagen de bordes
+    cv2.imshow('Bordes Canny', edges)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()'''
 
-# Liberar la cámara y cerrar las ventanas
-cap.release()
+def hysteresis_thresholding(image, low_threshold, high_threshold):
+    # Convertir la imagen a escala de grises si no lo está
+    if len(image.shape) > 2:
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Aplicar un filtro de mediana para reducir el ruido
+    image = cv2.medianBlur(image, 5)
+
+    # Aplicar el umbral alto y bajo
+    image_low = cv2.threshold(image, low_threshold, 255, cv2.THRESH_BINARY)[1]
+    image_high = cv2.threshold(image, high_threshold, 255, cv2.THRESH_BINARY)[1]
+
+    # Crear una imagen en blanco para guardar los bordes finales
+    edges = np.zeros_like(image)
+
+    # Definir los píxeles que están entre los umbrales como bordes potenciales
+    edges[(image >= low_threshold) & (image <= high_threshold)] = 255
+
+    # Utilizar la función cv2.findContours para encontrar los contornos de los bordes potenciales
+    contours, _ = cv2.findContours(edges, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+    # Filtrar los contornos por área y agregar los bordes finales a la imagen de salida
+    for contour in contours:
+        area = cv2.contourArea(contour)
+        if area > 50:
+            cv2.drawContours(image, [contour], 0, 255, -1)
+
+    return image
+
+# Lista de nombres de archivos de imagen
+image_files = ["edificio1.jpg","edificio2.jpg","edificio3.jpg","edificio4.jpg","edificio5.jpg"]
+
+# Umbral bajo y alto
+low_threshold = 50
+high_threshold = 150
+
+# Procesar cada imagen
+for image_file in image_files:
+    # Leer la imagen
+    image = cv2.imread(image_file)
+
+    # Aplicar el algoritmo de Hysteresis Thresholding
+    edges = hysteresis_thresholding(image, low_threshold, high_threshold)
+
+    # Mostrar la imagen de salida
+    cv2.imshow("Edges", edges)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+#Operaciones morfológicas
+
+# Cargando la imagen
+img = cv2.imread('edificio1.jpg', 0)
+
+# Definiendo el kernel (estructura) para la operación de erosión
+kernel = np.ones((5,5), np.uint8)
+
+# Aplicando la operación de erosión
+erosion = cv2.erode(img, kernel, iterations=1)
+
+# Mostrando la imagen original y la imagen erosionada
+cv2.imshow('Imagen original', img)
+cv2.imshow('Imagen erosionada', erosion)
+
+# Esperando a que se presione una tecla para cerrar las ventanas
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#Operación de dilatación
+# Cargando la imagen
+img = cv2.imread('edificio2.jpg', 0)
+
+# Definiendo el kernel (estructura) para la operación de dilatación
+kernel = np.ones((5,5), np.uint8)
+
+# Aplicando la operación de dilatación
+dilatacion = cv2.dilate(img, kernel, iterations=1)
+
+# Mostrando la imagen original y la imagen dilatada
+cv2.imshow('Imagen original', img)
+cv2.imshow('Imagen dilatada', dilatacion)
+
+# Esperando a que se presione una tecla para cerrar las ventanas
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#Operación de apertura
+# Cargando la imagen
+img = cv2.imread('edificio3.jpg', 0)
+
+# Definiendo el kernel (estructura) para la operación de apertura
+kernel = np.ones((5,5), np.uint8)
+
+# Aplicando la operación de apertura
+opening = cv2.morphologyEx(img, cv2.MORPH_OPEN, kernel)
+
+# Mostrando la imagen original y la imagen con apertura
+cv2.imshow('Imagen original', img)
+cv2.imshow('Imagen con apertura', opening)
+
+# Esperando a que se presione una tecla para cerrar las ventanas
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+#Operación de cerradura 
+# Cargando la imagen
+img = cv2.imread('edificio4.jpg', 0)
+
+# Definiendo el kernel (estructura) para la operación de cerradura
+kernel = np.ones((5,5), np.uint8)
+
+# Aplicando la operación de cerradura
+closing = cv2.morphologyEx(img, cv2.MORPH_CLOSE, kernel)
+
+# Mostrando la imagen original y la imagen con cerradura
+cv2.imshow('Imagen original', img)
+cv2.imshow('Imagen con cerradura', closing)
+
+# Esperando a que se presione una tecla para cerrar las ventanas
+cv2.waitKey(0)
 cv2.destroyAllWindows()
